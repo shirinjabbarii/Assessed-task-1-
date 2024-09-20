c5fb24a3049b063269f803e2b9c233f809ca67ec
shirinjabbarii <87862187+shirinjabbarii@users.noreply.github.com>
2024-09-20 05:56:22 +0000

    fix one mistake from fix-ner.sh script

--- fix-ner.sh
+++ fix-ner.sh
@@ -5,4 +5,4 @@ input_file=$1
 output_file=$2
 
 # Replace NER tags for punctuation with "O"
-sed 's/[[:punct:]]\/[A-Z]\+/\0\/O/g' $input_file > $output_file
+sed 's|\([.,;?!:]\)/[^O]|\1/O|g' $input_file > $output_file
